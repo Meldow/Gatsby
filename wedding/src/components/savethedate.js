52f974107b441a215a2f59fc89b397e5e8e37428
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-26 19:39:21 +0000

    feat(Start): Start wedding website
    Add instagram empty component
    Add location empty component
    Add savethedate empty component
    Add main empty component

---
+++ wedding/src/components/savethedate.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default () => (
+    <div>
+        Save the date
+    </div>
+)
--- wedding/src/pages/index.js
+++ wedding/src/pages/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Main from '../components/main'
+import Instagram from '../components/instagram'
+import Savethedate from '../components/savethedate'
+import Location from '../components/location'
 
 export default () => (
     <Layout>
-        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-        <p>
-            What do I like to do? Lots of course but definitely enjoy building
-            websites.
-        </p>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <Main />
+        <Savethedate />
+        <Instagram />
+        <Location />
     </Layout>
 )
