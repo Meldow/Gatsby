78f71d9c14c05e3319bafa3147f240d0967bbc34
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-26 19:04:25 +0000

    feat(Styling): Add styling

--- wedding/src/pages/index.js
+++ wedding/src/pages/index.js
@@ -6,6 +6,9 @@ export default () => (
         <h1>Hello Gatsby!</h1>
         <p>What a world.</p>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <br/>
         <Link to="/about/">About</Link>
+        <br/>
+        <Link to="/about-css-modules/">About css</Link>
     </div>
 )
