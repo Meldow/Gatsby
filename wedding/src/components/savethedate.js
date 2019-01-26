3a0cb1659e3cb4ed270d54b5add7a31204ab5a9b
Alexandre Costa <alexandre.costa@farfetch.com>
2019-01-26 23:58:26 +0000

    feat(SaveTheDate): Improve save the date component

--- wedding/src/components/savethedate.js
+++ wedding/src/components/savethedate.js
@@ -1,19 +1,45 @@
 import React from 'react'
 import savethedateStyles from './savethedate.module.css'
-import { Container, Grid, Card, Icon, Image } from 'semantic-ui-react'
+import { Container, Grid, Card, Icon, Image, Segment } from 'semantic-ui-react'
 
+const alightRight = {
+    marginLeft: 'auto',
+    marginRight: 0,
+}
 export default () => (
-    <div className={ savethedateStyles.letter_font }>
-        Save the date
-        <Grid>
-            <Grid.Row>
-                1
+    <div className={ savethedateStyles.letter_font_small }>
+        <Grid container centered columns={ 2 }>
+            <Grid.Row columns={ 2 }>
+                <Grid.Column textAlign='right'>
+                    <Grid.Row
+                        className={ savethedateStyles.letter_font_small }
+                        style={ { height: '3rem' } }>
+                        Save
+                    </Grid.Row>
+                    <Grid.Row
+                        className={ savethedateStyles.letter_font_small }
+                        style={ { height: '3rem' } }>
+                        the
+                    </Grid.Row>
+                    <Grid.Row
+                        className={ savethedateStyles.letter_font_small }
+                        style={ { height: "3rem" } }>
+                    date
+                    </Grid.Row>
+                </Grid.Column>
+                <Grid.Column
+                    textAlign='left'
+                    className={ savethedateStyles.numeric_font_big }>
+                    24
+                </Grid.Column>
             </Grid.Row>
-            <Grid.Row>
-                2
+            <Grid.Row
+                className={ savethedateStyles.letter_font_big }>
+                August
             </Grid.Row>
-            <Grid.Row>
-                3
+            <Grid.Row
+                className={ savethedateStyles.numeric_font_normal }>
+                2019
             </Grid.Row>
         </Grid>
 
